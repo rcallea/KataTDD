d7f30d0f020bf78912df1c649ad41fe34ef9ae02
Ricardo <cr.calle@uniandes.edu.co>
2020-08-20 18:21:03 -0500

    Caso 1: Calcular promedio con un conjunto vacío (Rojo)

--- tests/test_conjunto.py
+++ tests/test_conjunto.py
@@ -2,3 +2,7 @@ import unittest
 from KataTDD.conjunto import Conjunto
 
 class TestConjunto(unittest.TestCase):
+
+    def test_conjunto_vacio(self):
+        conjunto = Conjunto([])
+        self.assertIsNone(conjunto.promedio())
